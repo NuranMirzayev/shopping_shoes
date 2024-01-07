5d1fb48a35bf3368cf82c3b29779b8434c3e59be
Nuran Mirzayev <nuran.mirzayev.98@mail.ru>
2024-01-07 04:25:37 +0200

    fix

--- src/components/Side_Modal/SideModals.tsx
+++ src/components/Side_Modal/SideModals.tsx
@@ -1,122 +1,125 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import Button from '@mui/material/Button'
 
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import Button from '@mui/material/Button';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { RootState } from '../../app/store'
+import { getAllTotal } from '../../features/ShoppingCart/CartSlice'
+import Items from './Items'
 
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from "react-toastify";
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { RootState } from '../../app/store';
-import { getAllTotal } from '../../features/ShoppingCart/CartSlice';
-import Items from './Items';
-
-import './side.css';
-import { useAuth } from '../../app/useAuth';
-
+import { useAuth } from '../../app/useAuth'
+import './side.css'
 
 interface SideModal {
-    cartOpen: boolean;
-    setCartOpen: (cartOpen: boolean) => void;
+	cartOpen: boolean
+	setCartOpen: (cartOpen: boolean) => void
 }
 
-
-
 const SideModals = ({ setCartOpen, cartOpen }: SideModal) => {
-    const dispatch = useAppDispatch()
+	const dispatch = useAppDispatch()
 
-    const cartItems = useAppSelector((state: RootState) => state.cart.items);
+	const cartItems = useAppSelector((state: RootState) => state.cart.items)
 
-    const cart = useAppSelector(state => state.cart)
+	const cart = useAppSelector(state => state.cart)
 
-    const navigate = useNavigate()
+	const navigate = useNavigate()
 
-    const { isAuth } = useAuth();
+	const { isAuth } = useAuth()
 
+	useEffect(() => {
+		dispatch(getAllTotal())
+	}, [cart, dispatch])
 
-    useEffect(() => {
-        dispatch(getAllTotal())
-    }, [cart, dispatch])
+	const handleClickCheckOut = () => {
+		if (isAuth) {
+			if (cartItems.length === 1) {
+				navigate('/checkOut')
+			} else {
+				toast.warning(`Your cart is empty.`, {
+					position: 'bottom-left',
+				})
+			}
+		} else {
+			toast.warning(
+				`You are not Logged in,please Sign up or Login to your account `,
+				{
+					position: 'bottom-left',
+				}
+			)
+		}
+	}
 
-    const handleClickCheckOut = () => {
-        if (isAuth) {
-            if (cartItems.length === 1) {
-                navigate("/checkOut")
-            } else {
-                toast.warning(`Your cart is empty.`, {
-                    position: 'bottom-left'
-                })
-            }
-        } else {
-            toast.warning(`You are not Logged in,plese Sign up or Login to your account `, {
-                position: 'bottom-left'
-            })
-        }
-    }
+	return (
+		<div className={`window_cart ${cartOpen ? 'active' : ''}`}>
+			<Button
+				onClick={() => setCartOpen(false)}
+				className='p_delete'
+				color='inherit'
+			>
+				<ArrowBackIcon />
+			</Button>
+			<div className='scroll_box'>
+				{cartItems.length === 0 ? (
+					<h2
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+							marginTop: '60%',
+						}}
+					>
+						Your cart is empty.
+					</h2>
+				) : (
+					<Items />
+				)}
+			</div>
 
-        return (
-            <div className={`window_cart ${cartOpen ? "active" : ''}`}>
-                <Button onClick={() => setCartOpen(false)} className="p_delete" color="inherit">
-                    <ArrowBackIcon />
-                </Button>
-                <div className='scroll_box'>
-                    {cartItems.length === 0 ? (
-                        <h2 style={{ display: 'flex', justifyContent: 'center', marginTop: '60%' }}>Your cart is empty.</h2>
-                    )
-                        :
-                        <Items />
-                    }
-                </div>
+			<div className='bottomBox'>
+				<div className='amount_box'>
+					<h3>Total: ₪{cart.cartTotalAmount.toFixed(2)}</h3>
+					<h4>Delivery: Free</h4>
+				</div>
 
-                <div className="bottomBox">
+				<Button
+					disableElevation
+					variant='contained'
+					aria-label='Disabled elevation buttons'
+					className='textBtn'
+					sx={{
+						color: '#CD3333',
+						fontWeight: '600',
+						backgroundColor: '#white',
+						width: '50%',
+						margin: '0 auto',
+						marginBottom: '6px',
+					}}
+					color='inherit'
+					onClick={handleClickCheckOut}
+				>
+					CheckOut
+				</Button>
+				<Button
+					onClick={() => setCartOpen(false)}
+					disableElevation
+					variant='contained'
+					aria-label='Disabled elevation buttons'
+					className='textBtn'
+					sx={{
+						color: '#CD3333',
+						fontWeight: '600',
+						backgroundColor: 'white',
+						width: '50%',
+						margin: '0 auto',
+					}}
+					color='inherit'
+				>
+					Continue Shopping
+				</Button>
+			</div>
+		</div>
+	)
+}
 
-                    <div className='amount_box'>
-                        <h3 >Total: ₪
-                            {(cart.cartTotalAmount).toFixed(2)}
-                        </h3>
-                        <h4>Delivery: Free</h4>
-                    </div>
-
-                    <Button
-                        disableElevation variant="contained"
-                        aria-label="Disabled elevation buttons"
-                        className='textBtn'
-                        sx={
-                            {
-                                color: '#CD3333',
-                                fontWeight: '600',
-                                backgroundColor: '#white',
-                                width: '50%',
-                                margin: "0 auto",
-                                marginBottom: '6px'
-                            }
-                        }
-                        color='inherit'
-                        onClick={handleClickCheckOut}
-                    >
-                        CheckOut
-                    </Button>
-                    <Button
-                        onClick={() => setCartOpen(false)}
-                        disableElevation variant="contained"
-                        aria-label="Disabled elevation buttons"
-                        className='textBtn'
-                        sx={
-                            {
-                                color: '#CD3333',
-                                fontWeight: '600',
-                                backgroundColor: 'white',
-                                width: '50%',
-                                margin: "0 auto"
-                            }
-                        }
-                        color='inherit'
-                    >
-                        Continue Shopping
-                    </Button>
-                </div>
-
-            </div >
-        )
-    }
-
-    export default SideModals
+export default SideModals
